9fc3c5141cb5b793ebbee25e36d88f6c0b4277b4
HarutyunBadalyan <harutyun.badalyan@inomma.com>
2022-01-30 14:48:22 +0400

    improving database config.js

--- database/config/config.js
+++ database/config/config.js
@@ -1,6 +1,10 @@
 
-require("dotenv").config();
-console.log( process.env.DB_USERNAME)
+const path = require("path");
+require("dotenv").config()  
+if(!process.env.DB_USERNAME) {
+  require("dotenv").config({path: path.join(__dirname,"../","../",".env")})
+}
+console.log(process.env.DB_USERNAME,  process.env.DB_HOST)
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
